52af644803841dd778f23c6be8a1146c1033c8f9
Max Chistiakov <chistiakovmax@gmail.com>
2022-09-08 18:10:04 +0300

    All tasks are solved

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿//=======================================================================
+// Задача 13: Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+//=======================================================================
+
+Console.Write("Введите число: ");
+string inputNumber = Console.ReadLine()??"";
+int nLength = inputNumber.Length;
+int number = int.Parse(inputNumber);
+
+int inumber = nLength > 3 ? (int)(number / Math.Pow(10, (nLength - 3))) : number;
+Console.Write("Третья цифра: ");
+Console.WriteLine(inumber < 100 ? "третьей цифры нет" : inumber % 10);
