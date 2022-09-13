9de2d2e4faa60b8f4ee49fbda5dfecf6316ba039
Max Chistiakov <chistiakovmax@gmail.com>
2022-09-13 15:35:17 +0300

    Added tasks 25, 27, 29

---
+++ Sem4Task25/Program.cs
@@ -0,0 +1,43 @@
+﻿//=============================================================
+// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+//=============================================================
+
+// Math.Pow() работает существенно быстрее
+
+//Получаем данные от пользователя
+int ReadNumber(string text)
+{
+  Console.Write(text);
+  int res = int.Parse(Console.ReadLine()??"0");
+  return res;
+}
+
+//Выводим данные в консоль
+void DataOutput(string data)
+{
+  Console.WriteLine(data);
+}
+
+//Метод возводит число "a" в степень "b"
+long Exponent(int a, int b)
+{
+  long result = 1;
+  for(int i = 0; i < b; i++)
+  {
+    result = result * a;
+  }
+  return result;
+}
+
+int numA = ReadNumber("Введите число: ");
+int numB = ReadNumber("Введите степень: ");
+
+DateTime d1 = DateTime.Now;
+DataOutput("Вычисление через цикл: " + Exponent(numA, numB));
+Console.WriteLine("тест скорости: " + (DateTime.Now - d1));
+
+
+DateTime d2 = DateTime.Now;
+Console.WriteLine("Вычисление через Math.Pow: " + Math.Pow(numA, numB));
+Console.WriteLine("тест скорости: " + (DateTime.Now - d2));
---
+++ Sem4Task29/Program.cs
@@ -0,0 +1,38 @@
+﻿//=================================================================================
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//=================================================================================
+
+//Получаем данные от пользователя
+int ReadNumber(string text)
+{
+  Console.Write(text);
+  int res = int.Parse(Console.ReadLine()??"0");
+  return res;
+}
+
+//Выводим данные в консоль
+void DataOutput(string data)
+{
+  Console.WriteLine(data);
+}
+
+int length = ReadNumber("Какой длины будет массив?: ");
+int fromNum = ReadNumber("Интервал значений, от: ");
+int toNum = ReadNumber("до: ");
+
+// Функция создает массив заданной длины, с рандомными элементами в заданном интервале.
+string RandowArr(int lng, int from, int to)
+{
+  int[] array = new int[lng];
+  Random rnd = new Random();
+
+  for(int i = 0; i < length; i++)
+  {
+    array[i] = rnd.Next(from, to+1);
+  }
+
+  return string.Join(", ", array);
+}
+
+
+DataOutput("[" + RandowArr(length, fromNum, toNum) +"]");
