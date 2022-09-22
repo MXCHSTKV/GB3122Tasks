23723888f9cdaf4a9c601be702b3d3ddba6f66f0
Max Chistiakov <chistiakovmax@gmail.com>
2022-09-23 00:24:16 +0300

    Added tasks 41 and 43

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,33 @@
+﻿// ------------------------------------------------------
+// Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+// ------------------------------------------------------
+
+// Получение данных от пользователя и преобразование в число
+int ReadData(string text)
+{
+  Console.Write(text);
+  int number = int.Parse(Console.ReadLine()??"0");
+  return number;
+}
+
+// Вывод результата в консоль
+void PrintResult(string data)
+{
+    Console.WriteLine(data);
+}
+
+// Считаем положительные числа
+int Counter(int n)
+{
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int num = ReadData("Введите число: ");
+        if (num > 0) res++;
+    }
+
+    return res;
+}
+
+PrintResult($"Чисел больше нуля: {Counter(ReadData("Введите количество чисел: "))}");
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,37 @@
+﻿// ----------------------------------------------------------------------------
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2
+// задаются пользователем.
+// ----------------------------------------------------------------------------
+
+
+// Получение данных от пользователя и преобразование в число
+double ReadData(string text)
+{
+  Console.Write(text);
+  double number = double.Parse(Console.ReadLine()??"0");
+  return number;
+}
+
+// Вывод результата в консоль
+void PrintResult(string data)
+{
+    Console.WriteLine(data);
+}
+
+// Поиск точки пересечения двух прямых
+double[] SearchIntersection(double k1, double b1, double k2, double b2)
+{
+    double[] outArr = new double[2];
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    outArr[0] = x;
+    outArr[1] = y;
+    return outArr;
+}
+double b1 = ReadData("Введите b1: ");
+double k1 = ReadData("Введите k1: ");
+double b2 = ReadData("Введите b2: ");
+double k2 = ReadData("Введите k2: ");
+double[] intersection = SearchIntersection(k1, b1, k2, b2);
+PrintResult($"Точка пересечения прямых: ({Math.Round(intersection[0],2)};{Math.Round(intersection[1], 2)})");
